629a543d0036894d8aa4aa38742231738e5d4602
JeffDinsmore <hello@jeffdinsmore.com>
2020-09-24 17:48:08 -0700

    final test to see if a number will be transalted up to 999,999 and test had a good fail and a pass

--- NumbersToWords.Tests/ModelTests/TranslateTests.cs
+++ NumbersToWords.Tests/ModelTests/TranslateTests.cs
@@ -11,24 +11,47 @@ namespace NumbersToWords.Tests
     public void TranslateOnes_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
     {
       Translate testOnes = new Translate();
-      string number = "1";
-      int input = int.Parse(number);
-      Assert.AreEqual("one", testOnes.OneThruNineteen(input));
+      string number = "101";
+      Assert.AreEqual("one", testOnes.OneThruNineteen(number));
     }
 
     [TestMethod]
     public void TranslateTens_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
     {
       Translate testTens = new Translate();
-      string number = "20";
-      Assert.AreEqual("twenty", testTens.TensAfterNineteen(number));
+      string number = "3245";
+      Assert.AreEqual("forty", testTens.TensAfterNineteen(number));
     }
     [TestMethod]
     public void TranslateHundreds_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
     {
       Translate testHundreds = new Translate();
-      string number = "300";
+      string number = "74300";
       Assert.AreEqual("three hundred", testHundreds.OneHundreds(number));
+    }
+
+    [TestMethod]
+    public void TranslateOnesAndTens_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
+    {
+      Translate testMultipleTens = new Translate();
+      string number = "74";
+      Assert.AreEqual("seventy four", (testMultipleTens.TensAfterNineteen(number) + " " + testMultipleTens.OneThruNineteen(number)));
+    }
+
+    [TestMethod]
+    public void TranslatehundredsOfThousands_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
+    {
+      Translate testHundredsOfThousands = new Translate();
+      string number = "926000";
+      Assert.AreEqual("nine hundred twenty-six thousand", testHundredsOfThousands.OneThousands(number));
+    }
+
+    [TestMethod]
+    public void TranslateAllNumbersLessThanAMillion_TakeNumberAndTurnIntoWrittenNumber_WrittenNumber()
+    {
+      Translate testAllNumbersLessThanAMillion = new Translate();
+      string number = "94000";
+      Assert.AreEqual("nine hundred twenty-six thousand two hundred thirty-two", (testAllNumbersLessThanAMillion.OneThousands(number) + " " + testAllNumbersLessThanAMillion.OneHundreds(number) + " " + testAllNumbersLessThanAMillion.TensAfterNineteen(number) + " " + testAllNumbersLessThanAMillion.OneThruNineteen(number)));
     }
   }
 }
--- NumbersToWords/Models/Translate.cs
+++ NumbersToWords/Models/Translate.cs
@@ -6,29 +6,87 @@ namespace NumbersToWords.Models
   public class Translate
   {
 
-    public string OneThruNineteen (int onesInput)
+    public string OneThruNineteen (string onesInput)
     {
       Dictionary<int, string> OneThruNineteenValues = new Dictionary<int, string>() { {0, "zero"}, {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {11, "eleven"},  {12, "twelve"},  {13, "thirteen"},  {14, "fourteen"},  {15, "fifteen"},  {16, "sixteen"},  {17, "seventeen"},  {18, "eighteen"},  {19, "nineteen"} };
-
-      string output = OneThruNineteenValues[onesInput];
-      return output;
+      
+      if (onesInput.Substring((onesInput.Length-1),1) == "0" && onesInput.Length > 1)
+      {
+        string output = "";
+        return output;
+      }
+      else 
+      {
+        string ones = onesInput.Substring((onesInput.Length-1),1); 
+        int onesInteger = int.Parse(ones);
+        string output = OneThruNineteenValues[onesInteger];
+        return output;
+      }
+      
     }
     public string TensAfterNineteen (string tensInput)
     {
-      Dictionary<string, string> twentyThruNinetyNine = new Dictionary<string, string>() { {"2", "twenty"}, {"3", "thirty"}, {"4", "forty"}, {"5", "fifty"}, {"6", "sixty"}, {"7", "seventy"}, {"8", "eighty"}, {"9", "ninety"} };
+      Dictionary<string, string> twentyThruNinetyNine = new Dictionary<string, string>() { {"0", ""}, {"1", ""}, {"2", "twenty"}, {"3", "thirty"}, {"4", "forty"}, {"5", "fifty"}, {"6", "sixty"}, {"7", "seventy"}, {"8", "eighty"}, {"9", "ninety"} };
       
-      string tensNumber = tensInput.Substring(0,1);
-      string output = twentyThruNinetyNine[tensNumber];
-      return output; 
-
-    }
+        string tens = tensInput.Substring((tensInput.Length-2),1);
+        string output = twentyThruNinetyNine[tens];
+        return output;
+      }
+      
+    
     public string OneHundreds (string hundredsInput)
     {
-       Dictionary<string, string> oneThruNineHundred = new Dictionary<string, string>() { {"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"}, {"5", "five"}, {"6", "six"}, {"7", "seven"}, {"8", "eight"}, {"9", "nine"} };
+       Dictionary<string, string> oneThruNineHundred = new Dictionary<string, string>() { {"0", ""}, {"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"}, {"5", "five"}, {"6", "six"}, {"7", "seven"}, {"8", "eight"}, {"9", "nine"} };
       
-      string hundreds = hundredsInput.Substring(0,1);
+      string hundreds = hundredsInput.Substring((hundredsInput.Length-3),1);
       string output = oneThruNineHundred[hundreds];
-      return output + " hundred";
+      return output;
+    }
+
+    public string OneThousands (string thousandsInput)
+    {
+       Dictionary<string, string> onesOfThousands = new Dictionary<string, string>() { {"0", ""}, {"1", "one"}, {"2", "two"}, {"3", "three"}, {"4", "four"}, {"5", "five"}, {"6", "six"}, {"7", "seven"}, {"8", "eight"}, {"9", "nine"} };
+      
+      Dictionary<string, string> TensOfThousands = new Dictionary<string, string>() { {"0", ""}, {"1", "ten"}, {"11", "eleven"},  {"12", "twelve"},  {"13", "thirteen"},  {"14", "fourteen"},  {"15", "fifteen"},  {"16", "sixteen"},  {"17", "seventeen"},  {"18", "eighteen"},  {"19", "nineteen"}, {"2", "twenty"}, {"3", "thirty"}, {"4", "forty"}, {"5", "fifty"}, {"6", "sixty"}, {"7", "seventy"}, {"8", "eighty"}, {"9", "ninety"} };
+       
+      if (thousandsInput.Length == 4)
+      {
+        string thousands = thousandsInput.Substring((thousandsInput.Length-4),1);
+        string output = onesOfThousands[thousands];
+        return output + " thousand";
+      }
+      else if (thousandsInput.Length == 5)
+      {
+        if (int.Parse(thousandsInput.Substring((thousandsInput.Length-5),2)) <= 19 && int.Parse(thousandsInput.Substring((thousandsInput.Length-5),2)) >= 10)
+        {
+          string tenInThousands = thousandsInput.Substring((thousandsInput.Length-5),2);
+          string output = TensOfThousands[tenInThousands];
+          return output + " thousand";
+        }
+        else
+        {
+          string oneInThousands = thousandsInput.Substring((thousandsInput.Length-4),1);
+          string tenInThousands = thousandsInput.Substring((thousandsInput.Length-5),1);
+          string output = TensOfThousands[tenInThousands] + "-" + onesOfThousands[oneInThousands];
+          return output + " thousand";
+        }
+        
+      }
+      else if (thousandsInput.Length == 6)
+      {
+        string oneInThousands = thousandsInput.Substring((thousandsInput.Length-4),1);
+        string tenInThousands = thousandsInput.Substring((thousandsInput.Length-5),1);
+        string hundredInThousands = thousandsInput.Substring((thousandsInput.Length-6),1);
+        string output = onesOfThousands[hundredInThousands] + " hundred " + TensOfThousands[tenInThousands] + "-" + onesOfThousands[oneInThousands];
+        return output + " thousand";
+      }
+      else
+      {
+        string output = "";
+        return output;
+      }
+
+      
     }
   }
 }
